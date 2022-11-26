84b79b56177e755f2e85d7853014e484e66b905d
Najat451 <68127053+Najat451@users.noreply.github.com>
2022-11-26 21:32:02 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <SFML/Audio.hpp>
+using namespace sf;
+struct point{
+    int x,y;
+
+};
+int main(){
+    srand(time(0));
+    RenderWindow app(VideoMode(400,533),"Doodle Game");
+    app.setFramerateLimit(120);
+    Texture t1,t2,t3;
+    t1.loadFromFile("pic/background.jpg");
+    t2.loadFromFile("pic/platforme.jpg");
+    t3.loadFromFile("pic/doodle.jpg");
+    Sprite sbackground(t1), splat(t2), spers(t3);
+    point plat[20];
+    for(int i=0;i<10;i++)
+    {
+         plat[i].x=rand()%400;
+        plat[i].y=rand()%533;
+    }
+    int x=100,y=100,h=200;
+    float dy=0,dx=0;
+
+    while(app.isOpen()){
+        Event e;
+        while(app.pollEvent(e)){
+            if(e.type==Event::Closed)
+                app.close();
+        }
+    if(Keyboard::isKeyPressed(Keyboard::Right)) x+=3;
+    if(Keyboard::isKeyPressed(Keyboard::Left)) x-=3;
+
+
+
+    dy+=0.2;
+    y+=dy;
+    if(y>500) dy=-10;
+
+    if(y<h)
+    for(int i=0;i<10;i++){
+        y=h;
+        plat[i].y=plat[i].y-dy;
+        if(plat[i].y>533) {
+            plat[i].y=0;
+            plat[i].x=rand()%400;
+    }
+    }
+
+    for(int i=0;i<10;i++){
+        if((x+50>plat[i].x) && (x+20<plat[i].x+68) && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0))
+            dy=-10;
+    }
+    spers.setPosition(x,y);
+        app.draw(sbackground);
+        app.draw(spers);
+        for(int i=0;i<10;i++){
+            splat.setPosition(plat[i].x,plat[i].y);
+            app.draw(splat);
+        }
+        app.display();
+    }
+
+
+
+}
+
+
+
+
+
+
